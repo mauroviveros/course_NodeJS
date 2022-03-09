302e1e492b31889b77921f30023c6d4b5ee4853a
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-03-09 07:28:22 -0300

    feat:29. Inicio del proyecto - Sección 429. Inicio del proyecto - Sección 4

---
+++ 03_bases-nodejs/app.js
@@ -0,0 +1,9 @@
+const number = 5;
+console.clear();
+console.log("===============")
+console.log(`  Tabla del ${number}`)
+console.log("===============")
+
+for(i=1; i<=10; i++){
+    console.log(`${number} x ${i} = ${number * i}`);
+};
