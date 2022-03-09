092757dd4d355a838a6f946df96c305caa54a5ce
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-03-09 06:01:02 -0300

    feat:19. Funciones de Flecha

---
+++ 02-fundamentos/04_flecha.js
@@ -0,0 +1,9 @@
+function sumar(a, b){
+    return a + b;
+};
+
+const sumarArrow = (a, b) => a + b;
+const saludarArrow = () => "Hola Mundo";
+
+console.log(sumarArrow(5, 10));
+console.log(saludarArrow());
