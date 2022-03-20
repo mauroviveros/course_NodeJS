80c230177dd27c57cd7535c348ade420f12c538a
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-03-20 08:11:24 -0300

    feat:35. Configuraciones de Yargs

--- 03_bases-nodejs/02_require-fs.js
+++ 03_bases-nodejs/02_require-fs.js
@@ -1,13 +1,26 @@
 const fs = require("fs");
 const path = require("path");
 const colors = require("colors");
-const argv = require("yargs").argv;
+
 const printTable = require("./01_app-table");
+
+const argv = require("yargs").option("b", {
+    alias: "base",
+    type: "number",
+    default: 5,
+    description: "Asigna la Base de la Tabla"
+}).option("l", {
+    alias: "limite",
+    type: "number",
+    default: 10,
+    description: "Asigna el Limite de la Tabla"
+}).argv;
+
 
 const dir = path.join(__dirname, "./../tmp");
 
-let base = argv.base || 5;
-let limite = argv.limite || 10;
+let base = argv.base;
+let limite = argv.limite;
 
 let table = printTable(base, limite);
 
