df36d4348cb0e81115c9a97cf3c0f9da6f22a5df
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-05-12 15:33:38 -0300

    feat:87. Request y Response

--- 06_webserver/index.js
+++ 06_webserver/index.js
@@ -2,11 +2,21 @@
 const http = require("http");
 
 http.createServer((req, res)=>{
-    res.write("hola mundo");
-    res.write("hola mundo");
-    res.write("hola mundo");
-    res.write("hola mundo");
-    res.write("hola mundo");
+    
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Hola Mundo");
+
+    // res.writeHead(200, { "Content-Type": "application/json" });
+    // res.write(JSON.stringify({ id: 1, nombre: "Persona" }));
+
+    // res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
+    // res.writeHead(200, { "Content-Type": "application/csv" });
+    // res.write("id, nombre\n");
+    // res.write("1, Fernando\n");
+    // res.write("2, Mauro\n");
+    
+    
+
     res.end();
 }).listen(8080);
 
