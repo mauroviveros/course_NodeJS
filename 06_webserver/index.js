65209cc2296cc0f8532e318ba153dc8f1f9d813e
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-05-12 15:41:31 -0300

    feat:88. Introducción a EXPRESS

--- 06_webserver/index.js
+++ 06_webserver/index.js
@@ -1,23 +1,19 @@
 "use strict";
-const http = require("http");
+const express = require('express');
+const app = express();
+const PORT = 8080;
 
-http.createServer((req, res)=>{
-    
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.write("Hola Mundo");
+app.get('/', (req, res)=>{
+  res.send('Home page');
+});
+app.get('/hola', (req, res)=>{
+    res.send('Hello World');
+});
 
-    // res.writeHead(200, { "Content-Type": "application/json" });
-    // res.write(JSON.stringify({ id: 1, nombre: "Persona" }));
+app.get("*", (req, res)=>{
+    res.status(404).send("404 | Page Not Found");
+});
 
-    // res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
-    // res.writeHead(200, { "Content-Type": "application/csv" });
-    // res.write("id, nombre\n");
-    // res.write("1, Fernando\n");
-    // res.write("2, Mauro\n");
-    
-    
-
-    res.end();
-}).listen(8080);
-
-console.log("Escuchando el puerto: 8080");
+app.listen(PORT, ()=>{
+    console.log(`http://localhost:${PORT}`);
+});
