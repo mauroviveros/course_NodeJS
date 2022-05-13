08434e9d05bdaec00f4c4cadcd4451e330349a69
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-05-13 11:54:04 -0300

    fix:Se cambia el nombre del Repo 06

---
+++ 06_server-WEB/index.js
@@ -0,0 +1,45 @@
+"use strict";
+require("dotenv").config({ path: `${__dirname}/.env` });
+const path      = require('path');
+const hbs       = require('hbs');
+const express   = require('express');
+const app   = express();
+const PORT  = process.env.PORT;
+
+
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+app.set('views', path.join(__dirname,'/views'));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res)=>{
+    res.render("home", {
+        name: "Mauro Viveros",
+        title: "Curso NodeJS"
+    });
+});
+
+app.get('/generic', (req, res)=>{
+    res.render("generic", {
+        name: "Mauro Viveros",
+        title: "Curso NodeJS"
+    });
+});
+app.get('/elements', (req, res)=>{
+    res.render("elements", {
+        name: "Mauro Viveros",
+        title: "Curso NodeJS"
+    });
+});
+
+app.get("/react/*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "public/react/index.html"));
+});
+app.get("/angular/*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "public/angular/index.html"));
+});
+
+
+app.listen(PORT, ()=>{
+    console.log(`http://localhost:${PORT}`);
+});
