efeb18d52a807150f82c0eb5fc07938f4889d40a
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-05-13 11:28:53 -0300

    fix:Se actualiza el path del dotenv 06

--- 06_webserver/index.js
+++ 06_webserver/index.js
@@ -1,5 +1,5 @@
 "use strict";
-require("dotenv").config();
+require("dotenv").config({ path: `${__dirname}/.env` });
 const path      = require('path');
 const hbs       = require('hbs');
 const express   = require('express');
