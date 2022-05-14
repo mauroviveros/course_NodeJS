f0638dc3163b5c9770436555c7cd1f91f4860c17
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-05-14 10:58:04 -0300

    feat:119. Modelo de Usuario

---
+++ 07_server-REST/models/user.model.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const { Schema, model } = require("mongoose");
+
+const UserSchema = Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    img: {
+        type: String
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: [ "ADMIN_ROLE", "USER_ROLE" ]
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = model("User", UserSchema);
