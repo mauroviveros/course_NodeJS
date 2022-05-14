f0a4eeec352de44acff60deaf78071ef89b59f6f
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-05-14 09:42:49 -0300

    feat:107. Obtener datos de un POST

--- 07_server-REST/controllers/user.controller.js
+++ 07_server-REST/controllers/user.controller.js
@@ -3,12 +3,16 @@
 const usersGet = (req, res)=>{
     res.json({ msg: "get API" });
 };
+
 const usersPost = (req, res)=>{
-    res.status(201).json({ msg: "post API" });
+    const { name, age } = req.body;
+    res.status(201).json({ msg: "post API", name, age });
 };
+
 const usersPut = (req, res)=>{
     res.status(400).json({ msg: "put API" });
 };
+
 const usersDelete = (req, res)=>{
     res.status(500).json({ msg: "delete API" });
 };
