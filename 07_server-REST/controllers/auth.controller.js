0915a4896b49831b6339dbfd4bc3332fe09e8696
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-05-15 15:47:54 -0300

    feat:141. Login de usuario

--- 07_server-REST/controllers/auth.controller.js
+++ 07_server-REST/controllers/auth.controller.js
@@ -1,7 +1,25 @@
 "use strict";
 
-const login = (req, res)=>{
-    res.json({ message: "Login API" });
+const User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+
+const login = async (req, res)=>{
+    const body = req.body;
+    try{
+        const user = await User.findOne({ email: body.email, estado: true });
+        const valid = await bcrypt.compareSync(body.password, user.password);
+        if(!valid) throw new Error("invalid credentials");
+
+        // 4 Generar JWT
+
+
+        return res.json({
+            message: "OK"
+        });
+    } catch(error){
+        console.log(error);
+        return res.status(500).json({ message: error.message });
+    };
 };
 
 
