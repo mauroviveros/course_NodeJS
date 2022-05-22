516ce63be9ca220a1bfee8ff642955293bb6aa34
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-05-22 18:56:20 -0300

    feat:228. Diseño del login y su funcionamiento

---
+++ 07_server-REST/public/js/auth.js
@@ -0,0 +1,33 @@
+function handleCredentialResponse(response) {
+    const body = { google_token: response.credential };
+    const config = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+    };
+
+    fetch("http://localhost:8081/api/auth/login/google", config)
+        .then(resp => resp.json())
+        .then(resp => { localStorage.setItem("token", resp.token); })
+        .catch(console.warn);
+};
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    const formData = {};
+    for(let el of form.elements){
+        if(el.name.length > 0) formData[el.name] = el.value;
+    };
+
+    const config = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData)
+    };
+    fetch("http://localhost:8081/api/auth/login/", config)
+        .then(resp => resp.json())
+        .then(resp => { if(resp && resp.token) localStorage.setItem("token", resp.token); })
+        .catch(console.warn);
+})
