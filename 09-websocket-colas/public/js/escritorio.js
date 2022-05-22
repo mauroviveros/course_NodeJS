d831a9ec99b30d35ba14277e40cc5552940baa19
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-05-22 15:35:58 -0300

    feat:218. Preparar pantalla de escritorio

--- 09-websocket-colas/public/js/escritorio.js
+++ 09-websocket-colas/public/js/escritorio.js
@@ -1,3 +1,13 @@
+const searchParams = new URLSearchParams(window.location.search);
+const lblEscritorio = document.querySelector("#lblEscritorio");
+const lblTicket = document.querySelector("#lblTicket");
+const lblNoTickets = document.querySelector("#lblNoTickets");
+const lblPendientes = document.querySelector("#lblPendientes");
+const btn = document.querySelector("#attendTicket");
 
+if(!searchParams.has("escritorio")){
+    window.location = "/";
+    throw new Error("el Escritorio es obligatorio");
+};
 
-console.log('Escritorio HTML');
+lblEscritorio.innerText =  searchParams.get("escritorio");
