3894a52453569415dd877bd8034a81b65c1aeb95
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-05-22 16:18:12 -0300

    feat220. Mostrar cola de tickets en pantalla

--- 09-websocket-colas/api/sockets/controller.js
+++ 09-websocket-colas/api/sockets/controller.js
@@ -4,12 +4,13 @@ const ticketControl = new TicketControl();
 
 const connection = (socket)=>{
     socket.emit("last_ticket", ticketControl.last);
+    socket.emit("last_tickets", ticketControl.tickets4);
     socket.on("next_ticket", (payload, callback)=>{ callback(ticketControl.next()); });
     socket.on("attend_ticket", ({ escritorio }, callback)=>{
         if(!escritorio) return callback(false);
         const ticket = ticketControl.atenderTicket(escritorio);
-        console.log(ticket);
         if(!ticket) return callback(false);
+        socket.broadcast.emit("last_tickets", ticketControl.tickets4);
         return callback(ticket);
     });
 };
--- 09-websocket-colas/public/js/publico.js
+++ 09-websocket-colas/public/js/publico.js
@@ -1,3 +1,16 @@
+const socket = io();
+const lblTickets = [
+    { ticket: document.querySelector("#lblTicket1"), escritorio: document.querySelector("#lblEscritorio1") },
+    { ticket: document.querySelector("#lblTicket2"), escritorio: document.querySelector("#lblEscritorio2") },
+    { ticket: document.querySelector("#lblTicket3"), escritorio: document.querySelector("#lblEscritorio3") },
+    { ticket: document.querySelector("#lblTicket4"), escritorio: document.querySelector("#lblEscritorio4") }
+];
 
-
-console.log('Público HTML')
+socket.on("last_tickets", (tickets)=>{
+    lblTickets.map((lblTicket, index)=>{
+        if(tickets[index]){
+            lblTicket.ticket.innerText = `Ticket ${tickets[index].numero}`;
+            lblTicket.escritorio.innerText = `Escritorio: ${tickets[index].escritorio}`;
+        };
+    });
+});
