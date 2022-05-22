58bb9c1d450c52bd4672df60e6c17758bb0b2b0d
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-05-22 19:19:08 -0300

    feat:229. Validar el JWT - Servicio

--- 07_server-REST/routes/auth.routes.js
+++ 07_server-REST/routes/auth.routes.js
@@ -4,8 +4,10 @@ const { Router } = require("express");
 const router = Router();
 
 const authCtrl = require("../controllers/auth.controller");
+const { validarJWT } = require("../middlewares/jwt");
 
 router.post("/login", authCtrl.login);
 router.post("/login/google", authCtrl.googleLogin);
+router.get("/", [validarJWT], authCtrl.validateLogin);
 
 module.exports = router;
