efcc7c83baa16436a99c1de72a8318d390d20179
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-05-22 17:52:24 -0300

    feat:227. Configurar Socket.io

---
+++ 07_server-REST/sockets/controller.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const { Socket } = require("socket.io"); //!
+
+
+const socketController = (socket = new Socket)=>{
+    console.log("cliente conectado", socket.id);
+};
+
+
+
+module.exports = socketController;
