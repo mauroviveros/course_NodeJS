a2aed485443b2d1f51f0c39b97b587be04aa605e
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-05-22 19:21:55 -0300

    feat:230. Validar Token - Frontend

--- 07_server-REST/public/js/chat.js
+++ 07_server-REST/public/js/chat.js
@@ -1,3 +1,22 @@
-const socket = io();
+// const socket = io();
 
-socket.on("connection");
+// socket.on("connection");
+
+const validarJWT = async()=>{
+    const token = localStorage.getItem("token");
+    if(!token) return window.location = "/";
+    try{
+        const resp = await fetch("http://localhost:8081/api/auth/", { headers:{ Authorization: token } })
+        const { user, token: tokenDB } = await resp.json()
+        localStorage.setItem("token", tokenDB);
+    } catch(error){
+        console.warn(error);
+    };
+
+
+};
+
+const main = async()=>{
+    await validarJWT();
+};
+main();
